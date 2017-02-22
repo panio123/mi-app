6b56c06b9af4b9a00f3f94a20432615a69b1c47b
liupan <liupan@foreveross.com>
2017-02-22 14:39:56 +0800

    no msg

---
+++ src/components/home/list-two.vue
@@ -0,0 +1,67 @@
+<template>
+  <div class="list_two_type1">
+    <div class="list-box" v-for="item in list">
+      <img :src="item.img_url" alt="item.product_name">
+      <div class="list-box-bottom">
+        <h4>{{item.product_name}}</h4>
+        <h5>{{item.product_brief}}</h5>
+        <span>{{item.product_price}}</span>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+  export default{
+    name:'vue-list-two',
+    props:{
+      list:{
+        type:Array,
+        default:[]
+      }
+    },
+    data(){
+      return {}
+    }
+  }
+</script>
+<style lang="less">
+  @import '../../assets/css/constructor.less';
+
+  .list-box{
+  .scflex;
+  .vflex;
+    width: 3.6rem;
+    height: auto;
+    float: left;
+  .list-box-bottom{
+    padding: .2rem .27rem;
+  }
+  h4{
+    font-size: .28rem;
+    color: rgba(0,0,0,.87);
+    white-space: nowrap;
+    overflow: hidden;
+    text-overflow: ellipsis;
+  }
+  h5{
+    margin-top: .06rem;
+    font-size: .23rem;
+    line-height: .3rem;
+    color: rgba(0,0,0,.54);
+    white-space: nowrap;
+    overflow: hidden;
+    text-overflow: ellipsis;
+  }
+  span{
+    font-size: .32rem;
+    color: #ff6000;
+    margin-top: .08rem;
+  &:before{
+     content: "￥";
+     font-size: .2rem;
+     margin-right: .05rem;
+   }
+  }
+
+  }
+</style>
--- src/page/home/Home.vue
+++ src/page/home/Home.vue
@@ -17,16 +17,7 @@
         <vue-broadcast v-if="section.view_type === 'list_broadcast'" :list="section.body.items" :logo="section.body.title_logo_url"></vue-broadcast>
         <vue-line v-else-if="section.view_type === 'divider_line'" :height="section.body.line_height" :bgcolor="section.body.line_color"></vue-line>
         <vue-auto-img v-else-if="section.view_type === 'cells_auto_fill'" :list="section.body"></vue-auto-img>
-        <div class="list_two_type1" v-if="section.view_type === 'list_two_type1'">
-          <div class="list-box" v-for="item in section.body.items">
-            <img :src="item.img_url" alt="item.product_name">
-            <div class="list-box-bottom">
-              <h4>{{item.product_name}}</h4>
-              <h5>{{item.product_brief}}</h5>
-              <span>{{item.product_price}}</span>
-            </div>
-          </div>
-        </div>
+        <vue-list-two v-else-if="section.view_type === 'list_two_type1'" :list="section.body.items"></vue-list-two>
       </div>
     </div>
   </div>
@@ -37,13 +28,15 @@
   import VueBroadcast from 'components/home/broadcast.vue'
   import VueLine from 'components/home/line.vue'
   import VueAutoImg from 'components/home/autoImg.vue'
+  import VueListTwo from 'components/home/list-two.vue'
   export default {
     name: 'home',
     components: {
       Swiper,
       VueBroadcast,
       VueLine,
-      VueAutoImg
+      VueAutoImg,
+      VueListTwo
     },
     data() {
       return {
@@ -157,44 +150,6 @@
   .section-box{
     position: relative;
     overflow: hidden;
-
-  .list-box{
-    .scflex;
-    .vflex;
-    width: 3.6rem;
-    height: auto;
-    float: left;
-    .list-box-bottom{
-      padding: .2rem .27rem;
-    }
-    h4{
-      font-size: .28rem;
-      color: rgba(0,0,0,.87);
-      white-space: nowrap;
-      overflow: hidden;
-      text-overflow: ellipsis;
-    }
-    h5{
-      margin-top: .06rem;
-      font-size: .23rem;
-      line-height: .3rem;
-      color: rgba(0,0,0,.54);
-      white-space: nowrap;
-      overflow: hidden;
-      text-overflow: ellipsis;
-    }
-    span{
-      font-size: .32rem;
-      color: #ff6000;
-      margin-top: .08rem;
-      &:before{
-          content: "￥";
-         font-size: .2rem;
-         margin-right: .05rem;
-       }
-    }
-
-  }
   }
 
 
